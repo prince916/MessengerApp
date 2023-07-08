b613004f732f8195c68767b7932e42d49bd66c6c
Your Name <you@example.com>
2023-07-08 23:44:29 +0530

    new commit

---
+++ src/component/Chats.jsx
@@ -0,0 +1,45 @@
+import { onSnapshot } from 'firebase/firestore';
+import React from 'react'
+import { db } from '../firebase';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { doc } from 'firebase/firestore';
+
+const Chats = () => {
+
+  const [chats,setChats] = useState([])
+
+  useEffect(()=>{
+    const unsub = onSnapshot(doc(db, "cities", "SF"), (doc) => {
+      console.log("Current data: ", doc.data());
+  });
+  
+  },[])
+  return (
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOL-QsACtKYTeFUrPj3OGMr_jDB4B1F76wtQ&usqp=CAU" alt="" />
+        <div className="userChatInfo">
+          <span>Prince</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOL-QsACtKYTeFUrPj3OGMr_jDB4B1F76wtQ&usqp=CAU" alt="" />
+        <div className="userChatInfo">
+          <span>Prince</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOL-QsACtKYTeFUrPj3OGMr_jDB4B1F76wtQ&usqp=CAU" alt="" />
+        <div className="userChatInfo">
+          <span>Prince</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chats
